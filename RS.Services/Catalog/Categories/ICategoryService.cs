7d820f13249f54d3f2badfd0724ddc9fd7616568
Doji <kjsdoji@gmail.com>
2021-07-11 14:45:22 +0700

    Add services

---
+++ RS.Services/Catalog/Categories/ICategoryService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using RS.ViewModels.Catalog.Categories;
+
+namespace RS.Services.Catalog.Categories
+{
+    public interface ICategoryService
+    {
+        Task<List<CategoryVm>> GetAll(string languageId);
+
+        Task<CategoryVm> GetById(string languageId, int id);
+    }
+}
---
+++ RS.Services/System/Roles/IRoleService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RS.ViewModels.System.Roles;
+using System.Threading.Tasks;
+
+namespace RS.Services.System.Roles
+{
+    public interface IRoleService
+    {
+        Task<List<RoleVm>> GetAll();
+    }
+}
